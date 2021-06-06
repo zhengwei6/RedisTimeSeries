0fd4190b9e4fd7dc61554590bb9df5259418b029
Joseph <sajochiu@gmail.com>
2021-06-06 22:36:34 +0800

    qq

--- bin/linux-x64-release/rmutil/alloc.d
+++
@@ -1,4 +0,0 @@
-../../bin/linux-x64-release/rmutil/alloc.o: \
- ../../deps/RedisModulesSDK/rmutil/alloc.c \
- ../../deps/RedisModulesSDK/rmutil/alloc.h \
- ../../deps/RedisModulesSDK/rmutil/../redismodule.h
--- bin/linux-x64-release/src/compressed_chunk.d
+++
@@ -1,5 +0,0 @@
-../bin/linux-x64-release/src/compressed_chunk.o: compressed_chunk.c \
- common.h compressed_chunk.h generic_chunk.h consts.h redisgears.h \
- ../deps/RedisModulesSDK/redismodule.h \
- ../deps/RedisModulesSDK/rmutil/strings.h gorilla.h chunk.h \
- ../deps/RedisModulesSDK/rmutil/alloc.h
